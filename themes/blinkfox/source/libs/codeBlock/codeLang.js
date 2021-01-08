31eec5fe1d902adbbc734f08c6b0236606e31134
silverfox <songwenwen@outlook.com>
2021-01-08 19:35:55 +0800

    add post

--- themes/blinkfox/source/libs/codeBlock/codeLang.js
+++ themes/blinkfox/source/libs/codeBlock/codeLang.js
@@ -5,7 +5,7 @@ $(function () {
 
   $('pre').before($highlight_lang);
   $('pre').each(function () {
-    var code_language = $(this).attr('class');
+    var code_language = $(this).find('code').attr('class');
 
     if (!code_language) {
       return true;
@@ -14,7 +14,7 @@ $(function () {
 
     // 首字母大写
     // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-    
+
     $(this).siblings(".code_lang").text(lang_name);
   });
 });
